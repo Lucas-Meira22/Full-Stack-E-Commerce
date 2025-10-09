c83855599f1ab206efc4f078afe781f5c39d09d1
LucasMeira22 <lucasomeira@gmail.com>
2025-10-09 13:03:42 -0400

    Fix: Update Angular Components

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product-service';
+import { Product } from '../../common/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
 export class ProductList {
+  
+  products: Product [] = [];
+  constructor(private productService: ProductService) {}
 
+  ngOnInit() {
+    this.listProducts();
+  }
+  listProducts() {
+    this.productService.getProductList().subscribe(data => {
+      this.products = data;
+    });
+  }
 }
