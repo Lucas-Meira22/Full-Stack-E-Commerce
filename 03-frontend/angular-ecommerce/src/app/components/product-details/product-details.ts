23dae9fdee4707e914633f02db61b1018d00367d
LucasMeira22 <lucasomeira@gmail.com>
2025-11-18 00:14:41 -0500

    feat:function handling product detail

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product-service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../common/product';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css'
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit{
 
-  constructor(){}
+  product!: Product;
+
+  constructor(private productSevice: ProductService, private route: ActivatedRoute){}
 
   ngOnInit(){
-    
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+  handleProductDetails() {
+  //get the id param string. convert string to a numb using the + symbol
+  const prodId: number = +this.route.snapshot.paramMap.get('id')!;
+
+  this.productSevice.getProduct(prodId).subscribe(data => {
+    this.product = data;
+  } )
   }
 }
