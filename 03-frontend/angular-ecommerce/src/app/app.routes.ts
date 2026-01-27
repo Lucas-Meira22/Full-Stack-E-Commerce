68c4c6c7425dd0ff8dc20cd340ff212a81578267
LucasMeira22 <lucasomeira@gmail.com>
2026-01-26 21:11:58 -0500

    feat: new component cart-details | new route for cart-details

--- 03-frontend/angular-ecommerce/src/app/app.routes.ts
+++ 03-frontend/angular-ecommerce/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { ProductDetails } from './components/product-details/product-details';
+import { CartDetails } from './components/cart-details/cart-details';
 
 export const routes: Routes = [
     {path: 'products/:id', component: ProductDetails},
+    {path: 'cart-details', component: CartDetails},
     {path: 'search/:keyword', component: ProductList},
     {path: 'category/:id/:name', component: ProductList},
     {path: 'category/', component: ProductList},
